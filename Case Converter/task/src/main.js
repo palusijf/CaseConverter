5cbeb97895a92bb63b05a766983844bda528b52f
Palusijf <palusijf@yahoo.fr>
2022-09-27 21:15:54 +0200

    Step 3

--- Case Converter/task/src/main.js
+++ Case Converter/task/src/main.js
@@ -20,13 +20,20 @@ document.getElementById("sentence-case").addEventListener("click", function () {
     text.value = str.join('. ');
 });
 
-/*
- function titleCase(str) {
-  str = str.toLowerCase().split(' ');
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-  }
-  return str.join(' ');
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
-titleCase("I'm a little tea pot");
-* */
+
+document.getElementById("save-text-file").addEventListener("click", function () {
+    let myText = document.querySelector("textarea").value;
+    download("text.txt", myText);
+});
